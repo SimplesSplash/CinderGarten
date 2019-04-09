a14e8e99d1736ddb0159b897a1b3f14c5021602e
Alexey Sokolovskiy <alex1997.2010@mail.ru>
2019-04-09 15:32:35 +0800

    Завершение доменной модели

--- src/main/java/com/diplom/LK/domain/Group.java
+++ src/main/java/com/diplom/LK/domain/Group.java
@@ -5,16 +5,63 @@
  */
 package com.diplom.LK.domain;
 
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 /**
  *
  * @author Валерия
  */
 @Entity
+@Table(name = "groups")
 class Group {
 
     public Group() {
     }
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {
+                CascadeType.PERSIST,
+                CascadeType.MERGE
+            },
+            mappedBy = "groups")
+    private Set<Teacher> teachers;
+    
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(Set<Teacher> teachers) {
+        this.teachers = teachers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
--- src/main/java/com/diplom/LK/domain/Teacher.java
+++ src/main/java/com/diplom/LK/domain/Teacher.java
@@ -5,12 +5,16 @@
  */
 package com.diplom.LK.domain;
 
+import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 
@@ -20,26 +24,38 @@ import javax.persistence.PrimaryKeyJoinColumn;
  */
 @Entity
 public class Teacher {
-    
-    
-    @OneToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @PrimaryKeyJoinColumn
     private User user;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     public Teacher() {
     }
-    
+
     private String firstName;
-    
+
     private String lastName;
-    
+
     private String patronymic;
-    
-    private Position positionId;
+
+    private Position position;
+
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {
+                CascadeType.PERSIST,
+                CascadeType.MERGE
+            })
+    @JoinTable(name = "teachers_groups",
+            joinColumns = {
+                @JoinColumn(name = "teacherId")},
+            inverseJoinColumns = {
+                @JoinColumn(name = "groupId")}
+    )
+    private Set<Group> groups;
 
     public User getUser() {
         return user;
@@ -47,6 +63,14 @@ public class Teacher {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public Set<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(Set<Group> groups) {
+        this.groups = groups;
     }
 
     public Long getId() {
@@ -81,25 +105,14 @@ public class Teacher {
         this.patronymic = patronymic;
     }
 
-    public Position getPositionId() {
-        return positionId;
+    public Position getPosition() {
+        return position;
     }
 
-    public void setPositionId(Position positionId) {
-        this.positionId = positionId;
+    public void setPosition(Position position) {
+        this.position = position;
     }
 
-    public Group getGroupId() {
-        return groupId;
-    }
+    
 
-    public void setGroupId(Group groupId) {
-        this.groupId = groupId;
-    }
-    
-    private Group groupId;
-    
-    
-    
-    
 }
